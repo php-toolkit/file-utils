9b5e38cf91da6e6e06bc52026dfe3141c06a3887
inhere <in.798@qq.com>
2018-02-01 23:28:42 +0800

    update something...

--- test/boot.php
+++ test/boot.php
@@ -10,14 +10,14 @@ date_default_timezone_set('Asia/Shanghai');
 spl_autoload_register(function ($class) {
     $file = null;
 
-    if (0 === strpos($class,'MyLib\FileUtilUtil\Example\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('MyLib\FileUtilUtil\Example\\')));
+    if (0 === strpos($class,'MyLib\FileUtil\Example\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('MyLib\FileUtil\Example\\')));
         $file = dirname(__DIR__) . "/example/{$path}.php";
-    } elseif (0 === strpos($class,'MyLib\FileUtilUtil\Test\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('MyLib\FileUtilUtil\Test\\')));
-        $file = dirname(__DIR__) . "/{$path}.php";
-    } elseif (0 === strpos($class,'MyLib\FileUtilUtil\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('MyLib\FileUtilUtil\\')));
+    } elseif (0 === strpos($class,'MyLib\FileUtil\Test\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('MyLib\FileUtil\Test\\')));
+        $file = __DIR__ . "/{$path}.php";
+    } elseif (0 === strpos($class,'MyLib\FileUtil\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('MyLib\FileUtil\\')));
         $file = dirname(__DIR__) . "/src/{$path}.php";
     }
 
